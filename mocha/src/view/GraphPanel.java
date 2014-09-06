f3dcb835e65f5d4976c98fd5f32d39f40e4c688f
Nicki Ogg <oggnicki@yahoo.com>
2014-09-06 19:53:08 -0400

    Independent scaling

--- mocha/src/view/GraphPanel.java
+++ mocha/src/view/GraphPanel.java
@@ -40,9 +40,8 @@ public class GraphPanel extends JPanel {
 		if (values.isEmpty())
 			return;
 		
-		double maxValue = getMaxValue();
+		SensorValue maxValue = getMaxValue();
 		double xScale = ((double) GRAPH_WIDTH) / NUM_VALUES;
-		double yScale = GRAPH_HEIGHT / maxValue;
 		
 		SensorValue last = values.get(0);
 		for (int i = 1; i < values.size(); i++) {
@@ -52,21 +51,21 @@ public class GraphPanel extends JPanel {
 			
 			if (last != null && value != null) {
 				g2.setColor(View.ALPHA1_COLOR);
-				drawLine(lastX, last.alpha1 / 2.0, x, value.alpha1 / 2.0, yScale, g2);
+				drawLine(lastX, last.alpha1, x, value.alpha1, GRAPH_WIDTH / maxValue.alpha1, g2);
 				g2.setColor(View.ALPHA2_COLOR);
-				drawLine(lastX, last.alpha2, x, value.alpha2, yScale, g2);
+				drawLine(lastX, last.alpha2, x, value.alpha2, GRAPH_WIDTH / maxValue.alpha2, g2);
 				g2.setColor(View.BETA1_COLOR);
-				drawLine(lastX, last.beta1, x, value.beta1, yScale, g2);
+				drawLine(lastX, last.beta1, x, value.beta1, GRAPH_WIDTH / maxValue.beta1, g2);
 				g2.setColor(View.BETA2_COLOR);
-				drawLine(lastX, last.beta2, x, value.beta2, yScale, g2);
+				drawLine(lastX, last.beta2, x, value.beta2, GRAPH_WIDTH / maxValue.beta2, g2);
 				g2.setColor(View.DELTA_COLOR);
-				drawLine(lastX, last.delta / 100.0, x, value.delta / 100.0, yScale, g2);
+				drawLine(lastX, last.delta, x, value.delta, GRAPH_WIDTH / maxValue.delta, g2);
 				g2.setColor(View.GAMMA1_COLOR);
-				drawLine(lastX, last.gamma1, x, value.gamma1, yScale, g2);
+				drawLine(lastX, last.gamma1, x, value.gamma1, GRAPH_WIDTH / maxValue.gamma1, g2);
 				g2.setColor(View.GAMMA2_COLOR);
-				drawLine(lastX, last.gamma2, x, value.gamma2, yScale, g2);
+				drawLine(lastX, last.gamma2, x, value.gamma2, GRAPH_WIDTH / maxValue.gamma2, g2);
 				g2.setColor(View.THETA_COLOR);
-				drawLine(lastX, last.theta / 20.0, x, value.theta / 20.0, yScale, g2);
+				drawLine(lastX, last.theta, x, value.theta, GRAPH_WIDTH / maxValue.theta, g2);
 				g2.setColor(Color.BLACK);
 			}
 			
@@ -80,37 +79,37 @@ public class GraphPanel extends JPanel {
 		g2.drawLine(x1, GRAPH_HEIGHT - lastY, x2, GRAPH_HEIGHT - curY);
 	}
 	
-	private double getMaxValue() {
-		double max = 0;
+	private SensorValue getMaxValue() {
+		SensorValue max = new SensorValue();
 		Object[] valuesArray = values.toArray();
 		for (Object o : valuesArray) {
 			SensorValue value = (SensorValue) o;
 			if (value == null) {
 				continue;
 			}
-			if (value.alpha1 / 2.0 > max) {
-				max = value.alpha1 / 2.0;
+			if (value.alpha1 > max.alpha1) {
+				max.alpha1 = value.alpha1;
 			}
-			if (value.alpha2 > max) {
-				max = value.alpha2;
+			if (value.alpha2 > max.alpha2) {
+				max.alpha2 = value.alpha2;
 			}
-			if (value.beta1 > max) {
-				max = value.beta1;
+			if (value.beta1 > max.beta1) {
+				max.beta1 = value.beta1;
 			}
-			if (value.beta2 > max) {
-				max = value.beta2;
+			if (value.beta2 > max.beta2) {
+				max.beta2 = value.beta2;
 			}
-			if (value.delta / 100.0 > max) {
-				max = value.delta / 100.0;
+			if (value.delta > max.delta) {
+				max.delta = value.delta;
 			}
-			if (value.gamma1 > max) {
-				max = value.gamma1;
+			if (value.gamma1 > max.gamma1) {
+				max.gamma1 = value.gamma1;
 			}
-			if (value.gamma2 > max) {
-				max = value.gamma2;
+			if (value.gamma2 > max.gamma2) {
+				max.gamma2 = value.gamma2;
 			}
-			if (value.theta / 20.0 > max) {
-				max = value.theta / 20.0;
+			if (value.theta > max.theta) {
+				max.theta = value.theta;
 			}
 		}
 		return max;
