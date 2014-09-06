eb998f0ad9593a005a2ddc3fab753a54e6c3d1ae
Nicki Ogg <oggnicki@yahoo.com>
2014-09-06 01:29:43 -0400

    Wrote framework for main

--- mocha/src/main/Main.java
+++ mocha/src/main/Main.java
@@ -1,5 +1,8 @@
 package main;
 
+import matcher.PatternMatcher;
+import view.View;
+
 public class Main {
 	static {
 		try {
@@ -11,5 +14,27 @@ public class Main {
 	}
 	public static void main(String[] args) {
 		// Insert global logic here
+		View view = new View();
+		PatternMatcher patternMatcher = new PatternMatcher();
+		
+		/*
+		 * Open the connection
+		 * 
+		 * Get Connection ID
+		 * Connect
+		 */
+		
+		/*
+		 * Do magic calibration stuff
+		 */
+		
+		while (true) {
+			/*
+			 * Call ReadPacket to get a new packet
+			 * Create a SensorValue to hold the information
+			 * Fill the SensorValue with the data
+			 * Pass the SensorValue to the view and the patternMatcher
+			 */
+		}
 	}
 }
