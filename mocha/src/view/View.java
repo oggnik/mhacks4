5582622dad50146c1330d951d4b8be360a38859c
Nicki Ogg <oggnicki@yahoo.com>
2014-09-06 01:24:58 -0400

    Updated View

---
+++ mocha/src/view/View.java
@@ -0,0 +1,22 @@
+package view;
+
+import model.SensorValue;
+
+/**
+ * The Display
+ *
+ */
+public class View {
+	public View() {
+		
+	}
+	
+	/**
+	 * Pass a SensorValue to the view.
+	 * The view updates with the new values.
+	 * @param sensorValue A new SensorValue
+	 */
+	public void update(SensorValue sensorValue) {
+		
+	}
+}
