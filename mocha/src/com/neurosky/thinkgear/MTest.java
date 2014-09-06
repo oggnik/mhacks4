3ec87097d0deb7fab2cb1dabf983c0ac962eea9e
mtracy01 <matthetracy@gmail.com>
2014-09-06 00:58:59 -0400

    stuff

--- mocha/src/com/neurosky/thinkgear/MTest.java
+++ mocha/src/com/neurosky/thinkgear/MTest.java
@@ -1,5 +1,14 @@
 package com.neurosky.thinkgear;
 
 public class MTest {
-
+	public static void main(String[] args)
+	{
+		try {
+	    	System.load("C:\\thinkgear.dll");
+	    } catch (UnsatisfiedLinkError e) {
+	      System.err.println("Native code library failed to load.\n" + e);
+	      System.exit(1);
+	    }
+		
+	}
 }
