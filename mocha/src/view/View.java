96b92cd0fb9a0f003b7f3a7e5fce61085af0f8d4
Nicki Ogg <oggnicki@yahoo.com>
2014-09-06 01:50:11 -0400

    Dumb update method for view

--- mocha/src/view/View.java
+++ mocha/src/view/View.java
@@ -1,5 +1,7 @@
 package view;
 
+import com.neurosky.thinkgear.ThinkGear;
+
 import model.SensorValue;
 
 /**
@@ -17,6 +19,19 @@ public class View {
 	 * @param sensorValue A new SensorValue
 	 */
 	public void update(SensorValue sensorValue) {
-		
+		System.out.println(sensorValue.alpha1);
+		System.out.println(sensorValue.alpha2);
+		System.out.println(sensorValue.attention);
+		System.out.println(sensorValue.battery);
+		System.out.println(sensorValue.beta1);
+		System.out.println(sensorValue.beta2);
+		System.out.println(sensorValue.delta);
+		System.out.println(sensorValue.gamma1);
+		System.out.println(sensorValue.gamma2);
+		System.out.println(sensorValue.meditation);
+		System.out.println(sensorValue.poorSignal);
+		System.out.println(sensorValue.raw);
+		System.out.println(sensorValue.theta);
+		System.out.println("-----------------\n");
 	}
 }
