6b61a165536f0826463f2acfdfb4a7fd81b36842
XJi <xiaojingji5@gmail.com>
2014-09-06 20:16:42 -0400

    PatternMatcher.java

--- mocha/src/matcher/ColorPattern.java
+++ mocha/src/matcher/ColorPattern.java
@@ -9,6 +9,7 @@ public class ColorPattern {
 		/**
 		 * Get pattern for each color and add patterns to patternArray
 		 */
+		Pattern yellow = new Pattern();
 		Pattern red = new Pattern();
 		red.higheralpha1 = 0.2;
 		red.loweralpha1 = 0.15;
@@ -26,13 +27,29 @@ public class ColorPattern {
 		red.lowerdelta = 0.65;
 		red.highertheta = 0.8;
 		red.lowertheta = 0.75;
-		Pattern yellow = new Pattern();
+		yellow.higheralpha1 = 0.2;
+		yellow.loweralpha1 = 0.15;
+		yellow.higheralpha2 = 0.3;
+		yellow.loweralpha2 = 0.25;
+		yellow.higherbeta1 = 0.4;
+		yellow.lowerbeta1=0.35;
+		yellow.higherbeta2=0.5;
+		yellow.lowerbeta2=0.45;
+		yellow.highergamma1 = 0.55;
+		yellow.lowergamma1 = 0.51;
+		yellow.highergamma2 = 0.6;
+		yellow.lowergamma2 = 0.56;
+		yellow.higherdelta = 0.7;
+		yellow.lowerdelta = 0.65;
+		yellow.highertheta = 0.8;
+		yellow.lowertheta = 0.75;
 		Pattern blue = new Pattern();
 		
 		/**
 		 * Code needs to be optimized 
 		 */
 		patternArray.add(red);
+		patternArray.add(yellow);
 		//patternArray.add(yellow);
 		//patternArray.add(blue);
 		
